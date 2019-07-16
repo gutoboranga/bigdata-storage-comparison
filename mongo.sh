ca6b8b31fa6b031e7d3653aba683509933f18d14
Augusto Boranga <augusto.boranga@cpd.ufrgs.br>
2019-07-15 23:31:24 -0300

    first commit

---
+++ mongo.sh
@@ -0,0 +1,5 @@
+
+
+./bin/ycsb load mongodb -s -P ../workloads/1000/insert > ../results/1000/insert/couchbase_run.txt
+
+-s -P workloads/workloada -p mongodb.url="mongodb://127.0.0.1:27017" > load_mongodb.txt
